767237c260c9bb9fa11974840bbc00fa67cdb727
1247004718 <31641368+1247004718@users.noreply.github.com>
2025-06-10 23:48:57 +0800

    Create sock.sh

---
+++ sock.sh
@@ -0,0 +1,130 @@
+#!/bin/sh
+apt update -y
+###
+installXray() {
+wget -c -q  -P /etc/xray https://github.com/XTLS/Xray-core/releases/download/v25.6.8/Xray-linux-64.zip
+mkdir -p /etc/xray/conf
+unzip -o "/etc/xray/Xray-linux-64.zip" -d /etc/xray >/dev/null
+}
+
+###
+installxrayService() {
+execStart='/etc/xray/xray run -confdir /etc/xray/conf'
+if [[ -n $(find /bin /usr/bin -name "systemctl") ]]; then
+        rm -rf /etc/systemd/system/xray.service
+        touch /etc/systemd/system/xray.service
+        cat <<EOF >/etc/systemd/system/xray.service
+[Unit]
+Description=Xray Service
+Documentation=https://github.com/xtls
+After=network.target nss-lookup.target
+[Service]
+User=root
+ExecStart=${execStart}
+Restart=on-failure
+RestartPreventExitStatus=23
+LimitNPROC=infinity
+LimitNOFILE=infinity
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload && systemctl enable xray.service && systemctl start xray.service && echo " start xray "
+fi	
+}
+
+###
+xrayconf() {
+
+cat <<EOF > /etc/xray/conf/config.json
+{
+  "log": null,
+  "routing": {
+    "rules": [
+      {
+        "inboundTag": [
+          "api"
+        ],
+        "outboundTag": "api",
+        "type": "field"
+      },
+      {
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "blocked",
+        "type": "field"
+      },
+      {
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ],
+        "type": "field"
+      }
+    ]
+  },
+  "dns": null,
+  "inbounds": [
+    {
+      "listen": null,
+      "port": 12321,
+      "protocol": "socks",
+      "settings": {
+        "auth": "password",
+        "accounts": [
+          {
+            "user": "123",
+            "pass": "123"
+          }
+        ],
+        "udp": true,
+        "ip": "127.0.0.1"
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "security": "none",
+        "tcpSettings": {
+          "header": {
+            "type": "none"
+          }
+        }
+      },
+      "sniffing": {}
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "transport": null,
+  "policy": {
+    "system": {
+      "statsInboundDownlink": true,
+      "statsInboundUplink": true
+    }
+  },
+  "api": {
+    "services": [
+      "HandlerService",
+      "LoggerService",
+      "StatsService"
+    ],
+    "tag": "api"
+  },
+  "stats": {},
+  "reverse": null,
+  "fakeDns": null
+}
+EOF
+}
+
+installXray
+installxrayService
+xrayconf

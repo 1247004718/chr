922e39daef922e78383cce37444ef1b50f4ee0cd
1247004718 <31641368+1247004718@users.noreply.github.com>
2025-07-05 21:15:47 +0800

    Update l2tpipsec.sh

--- l2tpipsec.sh
+++ l2tpipsec.sh
@@ -24,7 +24,7 @@
 # - DO NOT use these special characters within values: \ " '
 
 YOUR_IPSEC_PSK='88888888'
-YOUR_USERNAME='AG'
+YOUR_USERNAME='lalala'
 YOUR_PASSWORD='88888888'
 
 # =====================================================

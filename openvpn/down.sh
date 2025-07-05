564a71192ee03c9543005d0c2bc0d80583a71b66
1247004718 <31641368+1247004718@users.noreply.github.com>
2025-07-05 17:10:27 +0800

    Update down.sh

--- openvpn/down.sh
+++ openvpn/down.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+LOG_FILE="/var/log/openvpn-down.log"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+ipnet=$(echo $4 | cut -d "." -f3)
+iptables -t nat -D POSTROUTING -s 172.31.$ipnet.0/24 -j SNAT --to-source $4
+ip rule del from 172.31.$ipnet.0/24 table vpn$ipnet
+sed -i "/vpn$ipnet/d" /etc/iproute2/rt_tables
+echo "[$TIMESTAMP] clear all ok."
+##autodelete
+##ip route del default table v1 via $4 dev $1
+exit 0
